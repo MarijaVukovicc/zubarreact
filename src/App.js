35d0e2d465f390632251981e7b9c6920ff787b74
Marija Vukovic <marija_vukovic@outlook.com>
2022-09-14 23:07:16 +0200

    dodata komponenta navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Footer from './komponente/Footer';
+import NavBar from './komponente/NavBar';
 
 function App() {
   return (
     <div>
+      <NavBar></NavBar>
      <Footer></Footer>
     </div>
   );
