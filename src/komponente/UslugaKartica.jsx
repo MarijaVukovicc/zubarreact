bf4898bd3b811a2fc4ad3143f2a006c3b2a0f9e6
Marija Vukovic <marija_vukovic@outlook.com>
2022-09-20 21:45:25 +0200

    dodata funkcionalnost pretrage

--- src/komponente/UslugaKartica.jsx
+++ src/komponente/UslugaKartica.jsx
@@ -8,7 +8,7 @@ function UslugaKartica({ usluga, dodajZain, mod, izbaciZain}) {
 
         <h1>{usluga.naziv}</h1>
         <p className="price">{usluga.cena}</p>
-        {mod == 1 ?
+        {mod === 1 ?
           <p><button onClick={() => dodajZain(usluga.id)}>Zainteresovan/-a sam</button></p>
           :
           <p><button onClick={() => izbaciZain(usluga.id)}>Nisam vise zainteresovan/-a</button></p>
--- src/komponente/Usluge.jsx
+++ src/komponente/Usluge.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import UslugaKartica from './UslugaKartica';
+import {BsSearch} from "react-icons/bs"
+import { useState } from "react";
 
-function Usluge({ usluge, dodajZain, izbaciZain }) {
+function Usluge({ usluge, dodajZain }) {
 
-
+    const [searchTerm, setSearchTerm] = useState('');
+    function handleInput(e) {
+        setSearchTerm(e.target.value.toLowerCase());
+        console.log(e.target.value);
+    }
 
     return (
-        <div className='sveUsluge'>
-            {usluge.map((u) => (<UslugaKartica key={u.id} usluga={u} dodajZain={dodajZain} mod={1} ></UslugaKartica>))}
+        <div>
+            <input className='search' type="text" placeholder='Pretraga...' onChange={handleInput}></input>  <BsSearch />
+            <div className='sveUsluge'>
+
+                {searchTerm === '' ?
+                    usluge.map((u) => (<UslugaKartica key={u.id} usluga={u} dodajZain={dodajZain} mod={1} ></UslugaKartica>))
+                    :
+                    usluge.filter((u) => (u.naziv.toLowerCase().includes(searchTerm)))
+                        .map((u) => (<UslugaKartica key={u.id} usluga={u} dodajZain={dodajZain} mod={1} ></UslugaKartica>))
+                }
+            </div>
         </div>
     )
-
 }
 
 export default Usluge;
